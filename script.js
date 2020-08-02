59555313a29ab1b1858e26f6ab2fcbbcc8e4f1b1
nicholasscabral <nicholasscabral@gmail.com>
2020-08-02 18:29:09 -0300

    adding files

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let quotes = [
-    'The best thing about a boolean is even if you are wrong, you are only off by a bit. (Anonymous)',
-    'Without requirements or design, programming is the art of adding bugs to an empty text file. (Louis Srygley)',
-    'Before software can be reusable it first has to be usable. (Ralph Johnson)',
-    'The best method for accelerating a computer is the one that boosts it by 9.8 m/s2. (Anonymous)',
-    'I think Microsoft named .Net so it wouldn’t show up in a Unix directory listing. (Oktal)',
-    'If builders built buildings the way programmers wrote programs, then the first woodpecker that came along would destroy civilization. (Gerald Weinberg)',
-    'There are two ways to write error-free programs; only the third one works. (Alan J. Perlis)',
-    'Ready, fire, aim: the fast approach to software development. Ready, aim, aim, aim, aim: the slow approach to software development. (Anonymous)',
-    'It’s not a bug – it’s an undocumented feature. (Anonymous)',
+    "Older men declare war. But it is the youth that must fight and die. — Herbert Hoover",
+    "Only the dead have seen the end of war. — Plato",
+    "Success is not final, failure is not fatal: it is the courage to continue that counts.— Winston Churchill",
+    "In war there is no prize for the runner-up. — General Omar Bradley",
+    "The soldier above all others prays for peace, for it is the soldier who must suffer and bear the deepest wounds and scars of war. — General Douglas MacArthur",
+    "If we don't end war, war will end us. — H. G. Wells",
+    "War does not determine who is right, only who is left. — Bertrand Russell",
+    "If your attack is going too well, you're walking into an ambush. — Infantry Journal",
 ]
 
 const button = document.querySelector('.button')
